58abb3ba8a47f94538b4d56a6627d93f3c04722a
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2023-09-28 10:57:37 +0300

    make ts error

--- src/server/controllers/main.controller.ts
+++ src/server/controllers/main.controller.ts
@@ -3,6 +3,7 @@ import { IRoute } from '../../../types/app';
 export const route: IRoute = {
   get: {
     '/': (req, res) => {
+      const x:string = 1;
       res.json({...req.query, test: 'test'});
     }
   }
